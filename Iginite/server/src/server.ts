b020db281e9a6c0c8b333317f6bd1b16f4b97bba
Vyckie <vyckie@aluno.ufrb.edu.br>
2022-11-01 00:34:22 -0300

    ending server day 1

--- Iginite/server/src/server.ts
+++ Iginite/server/src/server.ts
@@ -1,5 +1,6 @@
 import Fastify from 'fastify'
 import {PrismaClient} from '@prisma/client'
+import cors from '@fastify/cors'
 
 const prisma = new PrismaClient({
     log: ['query'],
@@ -10,6 +11,10 @@ async function bootstrap() {
         logger: true,
     })
 
+    await fastify.register(cors,{
+        origin: true,
+    })
+
     fastify.get('/pools/count', async ()=>{
        const pools = await prisma.pool.count()
 
@@ -17,7 +22,7 @@ async function bootstrap() {
         return { pools}
     })
 
-    await fastify.listen({port:3333})
+    await fastify.listen({port:3333, host: '0.0.0.0'})
 }
 
 bootstrap()
